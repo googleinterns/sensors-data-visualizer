a3d0d406861bde23e15cec8f464b10a9076b8551
Nick Aguilar <nickaguilar@google.com>
2020-07-09 23:30:10 +0000

    added licenses, changed sensor_id to str, initialize only one data channel, added documentation, renamed Sample.add_point data arg to datapoints to avoid confusion.

--- backend/parser.py
+++ backend/parser.py
@@ -1,3 +1,19 @@
+"""
+Copyright 2020 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 import re
 
 class Sample:
@@ -14,7 +30,7 @@ class Sample:
         next_index: Index where the next datapoint will be added for every list in the sample
     """
 
-    def __init__(self, sensor_name: str, sensor_id):
+    def __init__(self, sensor_name: str, sensor_id: str):
         """Initializes Sample with the sensor_name and a numeric sensor_id"""
         self.sensor_name = sensor_name
         self.sensor_id = sensor_id
@@ -26,9 +42,9 @@ class Sample:
         self.timestamp_diffs = []
 
         self.data = {}
-        self.data[0] = [] #x data
-        self.data[1] = [] #y data
-        self.data[2] = [] #z data
+        self.data[0] = [] 
+        self.data[1] = [] 
+        self.data[2] = [] 
 
         self.latencies = []
 
